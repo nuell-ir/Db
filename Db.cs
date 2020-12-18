633881930cf6c7ad45db287107117480be953364
nuell <newroz@gmail.com>
2020-12-18 21:17:37 +0330

    NewItem in vanilla SQL

--- Db.cs
+++ Db.cs
@@ -21,6 +21,32 @@ namespace nuell
                             return A;
                         }");
 
+        public static readonly string NewItem = 
+            @"select '{' + (
+                select '""' + COLUMN_NAME + '"":' + 
+                    case 
+                        when COLUMN_DEFAULT is null then
+                            case
+                                when IS_NULLABLE = 'YES' then 'null'
+                                else iif(DATA_TYPE in ('int', 'smallint', 'tinyint', 'bigint', 'float', 'real', 'bit'), '0', '""')
+                            end
+                        else
+                            case 
+                                when DATA_TYPE in ('int', 'smallint', 'tinyint', 'bigint', 'real', 'decimal', 'float') 
+                                    then substring(COLUMN_DEFAULT, 3, len(COLUMN_DEFAULT) - 4)
+                                when DATA_TYPE in ('bit')
+                                    then iif(upper(substring(COLUMN_DEFAULT, 3, len(COLUMN_DEFAULT) - 4)) in ('1', 'TRUE'), 'true', 'false')
+                                else '""' + iif(upper(left(COLUMN_DEFAULT, 3)) = '(N''', 
+                                    substring(COLUMN_DEFAULT, 4, len(COLUMN_DEFAULT) - 5), 
+                                    substring(COLUMN_DEFAULT, 3, len(COLUMN_DEFAULT) - 4)) + '""'
+                            end
+                    end + ','
+                from INFORMATION_SCHEMA.COLUMNS 
+                where TABLE_NAME = @table 
+                order by ORDINAL_POSITION
+                for xml path ('')
+            ) + '}'";
+
         public static SqlParameter NS(string name, string value)
             => new SqlParameter(name, string.IsNullOrWhiteSpace(value) ? null : value.Trim());
 
