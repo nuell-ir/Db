be793f1b47157f340022bec4d550de40e96b89bb
pellk <newroz@gmail.com>
2023-09-14 19:29:35 +0330

    feat: SaveAll

---
+++ SaveAll.cs
@@ -0,0 +1,156 @@
+using System.Text;
+using System.Text.Json;
+using System.Text.RegularExpressions;
+using Microsoft.Data.SqlClient;
+
+namespace nuell
+{
+	internal static partial class SaveAllQuery
+	{
+		internal static string Create(JsonElement json, string deleteIds, string table, string idProp)
+		{
+			var str = new StringBuilder("BEGIN TRAN;");
+
+			if (!string.IsNullOrWhiteSpace(deleteIds))
+			{
+				if (!CommaSeparatedIntegers().IsMatch(deleteIds))
+					throw new InvalidDataException("Wrong format of delete IDs");
+
+				str.Append("DELETE FROM ");
+				str.Append(table);
+				str.Append(" WHERE [");
+				str.Append(idProp);
+				str.Append("] in (");
+				str.Append(deleteIds);
+				str.Append(");");
+			}
+
+			var first = json[0];
+			var props = new List<string>();
+			var insertItems = new List<JsonElement>();
+			var updateItems = new List<(int Id, JsonElement Object)>();
+			foreach (var p in first.EnumerateObject())
+				if (p.Name != idProp)
+					props.Add(p.Name);
+
+			int id;
+			foreach (var itm in json.EnumerateArray())
+			{
+				id = itm.GetProperty(idProp).GetInt32();
+				if (id == 0)
+					insertItems.Add(itm);
+				else
+				{
+					updateItems.Add((id, itm));
+					continue;
+				}
+			}
+
+			if (insertItems.Count > 0)
+			{
+				str.Append("INSERT INTO ");
+				str.Append(table);
+				str.Append('(');
+				foreach (var prop in props)
+				{
+					str.Append('[');
+					str.Append(prop);
+					str.Append("],");
+				}
+				str.Remove(str.Length - 1, 1);
+				str.Append(") VALUES ");
+				foreach (var itm in insertItems)
+				{
+					str.Append('(');
+					foreach (string prop in props)
+					{
+						AppendValue(itm.GetProperty(prop));
+						str.Append(',');
+					}
+					str.Remove(str.Length - 1, 1);
+					str.Append("),");
+				}
+				str.Remove(str.Length - 1, 1);
+				str.Append(';');
+			}
+
+			foreach (var itm in updateItems)
+			{
+				str.Append("UPDATE ");
+				str.Append(table);
+				str.Append(" SET ");
+				foreach (string prop in props)
+				{
+					str.Append('[');
+					str.Append(prop);
+					str.Append("]=");
+					AppendValue(itm.Object.GetProperty(prop));
+					str.Append(',');
+				}
+				str.Remove(str.Length - 1, 1);
+				str.Append(" WHERE [");
+				str.Append(idProp);
+				str.Append("]=");
+				str.Append(itm.Id);
+				str.Append(';');
+			}
+
+			str.Append("COMMIT;");
+
+			return str.ToString();
+
+			void AppendValue(JsonElement e)
+			{
+				switch (e.ValueKind)
+				{
+					case JsonValueKind.Number:
+						str.Append(e);
+						break;
+					case JsonValueKind.String:
+						str.Append($"N'{e.GetString().Replace("'", "''")}'");
+						break;
+					case JsonValueKind.True:
+						str.Append(1);
+						break;
+					case JsonValueKind.False:
+						str.Append(0);
+						break;
+					case JsonValueKind.Null:
+						str.Append("NULL");
+						break;
+				}
+			}
+		}
+
+		[GeneratedRegex("^(\\d+,)*\\d+$")]
+		private static partial Regex CommaSeparatedIntegers();
+	}
+}
+
+namespace nuell.Sync
+{
+	public static partial class Db
+	{
+		public static int SaveAll(JsonElement json, string deleteIds, string table, string idProp = "Id")
+		{
+			using var cnnct = new SqlConnection(Data.ConnectionString);
+			using var cmnd = new SqlCommand(SaveAllQuery.Create(json, deleteIds, table, idProp), cnnct);
+			cnnct.Open();
+			return cmnd.ExecuteNonQuery();
+		}
+	}
+}
+
+namespace nuell.Async
+{
+	public static partial class Db
+	{
+		public static async Task<int> SaveAll(JsonElement json, string deleteIds, string table, string idProp = "Id")
+		{
+			using var cnnct = new SqlConnection(Data.ConnectionString);
+			using var cmnd = new SqlCommand(SaveAllQuery.Create(json, deleteIds, table, idProp), cnnct);
+			await cnnct.OpenAsync();
+			return await cmnd.ExecuteNonQueryAsync();
+		}
+	}
+}
